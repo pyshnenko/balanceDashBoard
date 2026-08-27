ef5d6d31c246f36c365bf6f2ef076644c140635e
pyshnenko <pyshnenko94@yandex.ru>
2026-08-27 11:48:37 +0300

    Update serverNames.ts

--- frontend/src/constans/serverNames.ts
+++ frontend/src/constans/serverNames.ts
@@ -1,6 +1,6 @@
 const serverNames = new Map<string, string> ([
     ['client_main', 'Миша основа'],
-    ['client_backup', 'Миша резерв'],,
+    ['client_backup', 'Миша резерв'],
     ['client_main_v6','Миша основа v6'],
     ['client_backup_v6','Миша резерв v6']
     ['v6_my', 'Мой сервер v6'],
