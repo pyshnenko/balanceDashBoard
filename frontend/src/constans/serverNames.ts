a46fdbb776b76bb0111bc5bd44a28f2584635f0e
pyshnenko <pyshnenko94@yandex.ru>
2026-08-27 11:47:00 +0300

    Update serverNames.ts

--- frontend/src/constans/serverNames.ts
+++ frontend/src/constans/serverNames.ts
@@ -1,6 +1,8 @@
 const serverNames = new Map<string, string> ([
     ['client_main', 'Миша основа'],
-    ['client_backup', 'Миша основа'],
+    ['client_backup', 'Миша резерв'],,
+    ['client_main_v6','Миша основа v6'],
+    ['client_backup_v6','Миша резерв v6']
     ['v6_my', 'Мой сервер v6'],
     ['v6_ts', 'ТС v6'],
     ['v6_codegap', 'codegap v6'],
@@ -12,7 +14,8 @@ const serverNames = new Map<string, string> ([
     ['aeza', 'aeza'],
     ['backend_5', 'codegap'],
     ['aeza_test_icmp', 'icmp'],
-    ['aeza_test_icmp_v6', 'icmp v6']
+    ['aeza_test_icmp_v6','icmp v6'],
+    ['codegag','codegag']
 ])
 
 export {serverNames}
